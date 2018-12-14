9a8dc6ee5ee68fb4ff188a2426c1f7c4a939ec6f
Gspartico <gspartico@gmail.com>
2018-12-14 10:39:51 -0500

    started header, created function to add box shadow across screen for name

---
+++ scripts/scripts.js
@@ -0,0 +1,32 @@
+$(function () {
+    let firstShadow = getShadow('#fa3297', 510, 'bottomLeft');
+    let secondShadow = getShadow('#ff4479', 400, 'bottomRight')
+    $('h1').attr('style', firstShadow);
+    $('h2').attr('style', secondShadow);
+});
+
+function getShadow(color, pxlength, direction) {
+    textShadow = [];
+    if (direction === 'topLeft') {
+        for (let i = 1; i < pxlength; i++) {
+            let str = `-${i}px -${i}px ${color}`;
+            textShadow.push(str);
+        }
+    } else if (direction === 'bottomRight') {
+        for (let i = 1; i < pxlength; i++) {
+            let str = `${i}px ${i}px ${color}`;
+            textShadow.push(str);
+        }
+    } else if (direction === 'bottomLeft') {
+        for (let i = 1; i < pxlength; i++) {
+            let str = `-${i}px ${i}px ${color}`;
+            textShadow.push(str);
+        }
+    } else {
+        for (let i = 1; i < pxlength; i++) {
+            let str = `${i}px ${i}px ${color}`;
+            textShadow.push(str);
+        }
+    }
+    return `text-shadow: ${textShadow.join()};`
+}
