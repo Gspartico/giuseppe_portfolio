a05dd88f2a5ccf17bdeb3484c8bef0471dd7506f
Gspartico <gspartico@gmail.com>
2018-12-15 17:47:44 -0500

    fixed up skills section, added svg hero text, added code for both draw on scroll and svg animation to decide on later, started works section

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -32,3 +32,20 @@ function getShadow(color, pxlength, direction) {
     }
     return `text-shadow: ${textShadow.join()};`
 }
+
+// const path = document.querySelector('#about', '#skills');
+// const pathLength = path.getTotalLength();
+// path.style.strokeDasharray = pathLength + ' ' + pathLength;
+// path.style.strokeDashoffset = pathLength;
+
+// window.addEventListener("scroll", function (e) {
+//     const scrollPercentage = (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
+//     const drawLength = pathLength * scrollPercentage;
+//     path.style.strokeDashoffset = pathLength - drawLength;
+// });
+
+// if (scrollPercentage >= 0.99) {
+//     path.style.strokeDasharray = "none";
+// } else {
+//     path.style.strokeDasharray = pathLength + ' ' + pathLength;
+// }
